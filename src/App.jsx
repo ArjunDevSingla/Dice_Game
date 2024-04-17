5bc0d477e58e1e2e18e21ede3e168969dabac2e9
Arjun Dev Singla <arjundevsingla1612@gmail.com>
2024-04-17 17:08:53 +0530

    first Commit - Made the Landing Page

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import LandingPage from "./components/LandingPage";
+import './App.css'
+import { useState } from "react";
+import GamePlay from "./components/GamePlay";
+
+function App() {
+
+  const [isGameStarted, setIsGameStarted] = useState(false);
+
+  const toggleGamePlay = () => {
+    setIsGameStarted((prev) => !prev);
+  }
+
+  return (
+    <div>
+      {isGameStarted ? <GamePlay /> : <LandingPage toggle = {toggleGamePlay} />};
+    </div>
+  )
+}
+
+export default App;
